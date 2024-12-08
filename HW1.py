54e4f30976b1154c2e19cb12b7fc9528b1bff410
b2112 <viacheslav.martson@gmail.com>
2024-12-08 17:55:43 +0200

    Lesson2

---
+++ HW1.py
@@ -0,0 +1,18 @@
+#№1004
+
+# num = float(input())
+# integer_num = int(num)
+# print (round(num))
+# print(integer_num)
+#print(f"{(num-integer_num):.1f}")
+
+#№1005
+
+# x,y = map(int,input().split())
+# res = x**3+(x+1)/(y**2+1)
+# print(f"{res:.1f}")
+
+#№1006
+
+#x,y = map(int,input().split())
+#print(x-y)
