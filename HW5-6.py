973fe334076095831bd5084f1de7434b7c99b067
b2112 <viacheslav.martson@gmail.com>
2024-12-22 16:27:34 +0200

    Lesson4

---
+++ HW5-6.py
@@ -0,0 +1,49 @@
+# 1023
+# x,y = map(int,input().split())
+# if x<0 and y>0:
+#  print("Yes")
+# else:
+#  print("No")
+
+# 1024
+# x,y = map(int,input().split())
+# if x>0 and y>0:
+#  print(1)
+# elif x<0 and y>0:
+#  print(2)
+# elif x<0 and y<0:
+#  print(3)
+# elif x>0 and y<0:
+#  print(4)
+
+# 1025
+# x,y,z = map(int,input().split())
+# if x+y>z and y+z>x and x+z>y:
+#  print("Yes")
+# else:
+#  print("No")
+
+# 1026
+# a,b,c,d = map(int,input().split())
+# if a<b<c<d:
+#  print("Yes")
+# else:
+#  print("No")
+
+# 1027
+
+# num = int(input())
+# a = num //1000
+# b = num //100%10
+# c = num //10%10
+# d = num % 10
+# print(max(a,b,c,d))
+
+# 1028
+# N=input()
+# kvadrat=int(N)**2
+# kub=(int(N[0])+int(N[1]))**3
+# if kvadrat==kub:
+#  print(int(N[0])+int(N[1]))
+# else:
+#  print(abs(int(N[0])-int(N[1])))
