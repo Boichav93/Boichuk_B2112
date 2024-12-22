bd2042cad2991b898ea3d3157b285c77c2be24cd
b2112 <viacheslav.martson@gmail.com>
2024-12-22 14:29:23 +0200

    HW3-4

---
+++ HW3-4.py
@@ -0,0 +1,17 @@
+#https://itstepedu-my.sharepoint.com/:u:/g/personal/boichuk_a_student_itstep_org/EcfbWsc3nt9Ng8972ZCzhhUBDQs0cNAkIrg8fZIJDShnZw?e=zJfDug
+# 1011
+# num=input()
+# print(num[2],num[1],num[0])
+
+#1012
+# a = int(input())
+# a = int(a)**3
+# print(a)
+
+# 1013
+# x,y=map(int,input().split())
+# print('%.2f'%((x+y)/2))
+
+# 1014
+# num=input()
+# print(num[1]+num[2]+num[0])
